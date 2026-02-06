a286edad4745682957b70e2eb127c3589a7faf3d
nhegge <nhegge@wisc.edu>
2026-02-05 18:05:54 -0600

    activity 2 final

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -1,57 +1,107 @@
 // Javascript written by Nolan Hegge, 1/23/2026
-// Add all scripts to the JS folder
+// Original Code Source: GEOG 575 - Activity 3
+// Credit to Chapter 2 for help with initialize() and cities() functions
 
-var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
-];
-
-function addColumns(cityPop){
-    
-    document.querySelectorAll("tr").forEach(function(row, i){
-
-    	if (i == 0){
-
-    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
-    	} else {
-
-    		var citySize;
-
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
-
-    		} else if (cityPop[i-1].population < 500000){
-    			citySize = 'Medium';
-
-    		} else {
-    			citySize = 'Large';
-    		};
-
-			row.insertAdjacntHTML('<td' + citySize + '</td>');
-    	};
-    });
+//initialize function called when the script loads
+function initialize(){
+	cities();
 };
 
+//function to create a table with cities and their populations
+function cities(){
+
+	//define an array of objects for cities and population
+	var cityPop = [
+		{ 
+			city: 'Madison',
+			population: 233209
+		},
+		{
+			city: 'Milwaukee',
+			population: 594833
+		},
+		{
+			city: 'Green Bay',
+			population: 104057
+		},
+		{
+			city: 'Superior',
+			population: 27244
+		}
+	];
+
+	//create a table element
+	var table = document.createElement('table');
+
+	//create a header row element and append it to the table
+	var headerRow = document.createElement('tr');
+	table.appendChild(headerRow);
+
+	//create the "City" and "Population" column headers
+	headerRow.insertAdjacentHTML('beforeend','<th>City</th><th>Population</th>');
+
+	//loop to add a new row for each city
+	cityPop.forEach(function(cityObject){
+
+		//assign longer html strings to a variable
+		var rowHtml = '<tr><td>' + cityObject.city + '</td><td>' + cityObject.population + '</td></tr>';
+
+		//add the row's html string to the table
+		table.insertAdjacentHTML('beforeend', rowHtml);
+	});
+
+	//append the table element into the div
+	document.querySelector('#mydiv').appendChild(table);
+
+	//then add third column and events after the table exists
+	addColumns(cityPop);
+	addEvents();
+};
+
+//function to add a new column to the table
+function addColumns(cityPop){
+
+	//loop to add a new column to each row
+	document.querySelectorAll('tr').forEach(function(row, i){
+
+		//for first row in the table, add the column header
+		if (i == 0){
+
+			row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
+
+		} else {
+
+			//determine city size based on population (small < 100000, medium < 500000, else large)
+			var citySize;
+
+			if (cityPop[i-1].population < 100000){
+				citySize = 'Small';
+
+			} else if (cityPop[i-1].population < 500000){
+				citySize = 'Medium';
+
+			} else {
+				citySize = 'Large';
+			};
+
+			//add new table cell with the city size
+			row.insertAdjacentHTML('beforeend', '<td>' + citySize + '</td>');
+		};
+	});
+};
+
+//function to add different mouse events to table (click/hover)
 function addEvents(){
 
-	document.querySelector("table").addEventListener("mouseover", function(){
-		
-		var color = "rgb(";
+	//select the table element
+	var table = document.querySelector('table');
 
+	//add a random color when there is mouseover (hovered over) on the table
+	table.addEventListener('mouseover', function(){
+
+		var color = 'rgb(';
+
+		//generate a random color
 		for (var i=0; i<3; i++){
 
 			var random = Math.round(Math.random() * 255);
@@ -59,20 +109,24 @@ function addEvents(){
 			color += random;
 
 			if (i<2){
-				color += ",";
-			
+				color += ',';
 			} else {
-				color += ")";
+				color += ')';
+			};
 		};
-	};
 
-		document.querySelector("table").color = color;
+		//style the table with the random color
+		table.style.color = color;
 	});
 
+	//function that sends an alert when table is clicked
 	function clickme(){
-
 		alert('Hey, you clicked me!');
 	};
 
-	document.querySelector("table").addEventListener("click", clickme)
+	//event listener that watches for a click on the table
+	table.addEventListener('click', clickme);
 };
+
+//once DOM has loaded then call initialize() function to run the code
+document.addEventListener('DOMContentLoaded', initialize);
